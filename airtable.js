e72af276adf4dd2084f62e17639fedeca973b368
Rafaelmarinholink <rafael.marinho@aluno.lsb.com.br>
2025-04-15 19:29:04 -0300

    Update airtable.js

--- airtable.js
+++ airtable.js
@@ -4,7 +4,7 @@ const baseId = 'appaq7tR3vt9vrN6y';         // Ex: app5AQ4goCRsCVNo9
 const tableName = 'Empresas';         // Certifique-se que a tabela tenha exatamente esse nome
 const apiKey = 'patAOGNbJyOQrbHPB.22dd0a4309dc09867d31612922b5616a0a83965352599929e3566187a84607c6';       // Cole aqui seu novo token (formato patxxxx...)
 
-const airtableURL = `https://airtable.com/appaq7tR3vt9vrN6y/tblE0CTAAfwmtrKtY/viwd10lGaeoadmux8?blocks=hide/${baseId}/${tableName}`;
+const airtableURL = `https://api.airtable.com/v0/${baseId}/${tableName}`;
 const headers = {
   Authorization: `Bearer ${apiKey}`,
   'Content-Type': 'application/json'
